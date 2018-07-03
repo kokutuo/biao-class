effa55ff68fdb04eadbc87d20981976ce621ce46
kokutuo <694888445@qq.com>
2018-07-03 19:47:57 +0800

    validator debug

--- project/010-yangyangche/src/directive/validator.js
+++ project/010-yangyangche/src/directive/validator.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue';
 
 export default Vue.directive('validator', {
@@ -8,13 +9,18 @@ export default Vue.directive('validator', {
             rule = parse_validator_rule(rule); // {required: true, min_length: 3}
         }
 
-        el.addEventListener('keyup', {
-            for (const r of rule) {
+        el.addEventListener('keyup', () => {
+            for (const r in rule) {
+                if (!rule.hasOwnProperty(r))
+                    continue;
+
                 let arg = rule[r];
                 let validator = valid[r];
 
-                if (validator && !validator(el.value)) {
-
+                if (validator && !validator(el.value, arg)) {
+                    console.log(r + "不合法");
+                } else {
+                    console.log(r + "合法");
                 }
             }
         })
@@ -31,7 +37,7 @@ function parse_validator_rule(str) { // 'required|min_length:3'
             let key = arr[0];
             let val = arr[1];
 
-            val = val === undefined ? true : val;
+            val = val == undefined ? true : val;
 
             rule[key] = val;
         });
@@ -40,17 +46,19 @@ function parse_validator_rule(str) { // 'required|min_length:3'
 }
 
 const valid = {
-    username() {},
+    // username() {},
 
     required(val) {
         return !!val;
     },
 
     min_length(val, min) {
+        console.log("min", min)
         return val.length >= min;
     },
 
     max_length(val, max) {
+        console.log("max", max)
         return val.length <= max;
     }
 };
