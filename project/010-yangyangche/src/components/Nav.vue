e66768521d73db7e8233e2de9f57da0b15b69fa6
kokutuo <694888445@qq.com>
2018-07-17 13:29:26 +0800

    logout

--- project/010-yangyangche/src/components/Nav.vue
+++ project/010-yangyangche/src/components/Nav.vue
@@ -10,8 +10,14 @@
         <SearchBar :width="'50%'"/>
       </div>
       <div class="col right">
-        <router-link class="nav-item" to="/login">登录</router-link>
-        <router-link class="nav-item" to="/signup">注册</router-link>
+        <span v-if="!uinfo">
+          <router-link class="nav-item" to="/login">登录</router-link>
+          <router-link class="nav-item" to="/signup">注册</router-link>
+        </span>
+        <span v-else>
+          <span class="nav-item">{{uinfo.username || uinfo.phone}}</span>
+          <span class="nav-item" @click="logout">登出</span>
+        </span>
         <a href="#" class="nav-item tel">400-6666-666</a>
       </div>
     </div>
@@ -21,6 +27,8 @@
 <script>
 import SearchBar from "./SearchBar.vue";
 
+import session from "../lib/session.js";
+
 export default {
   props: {
     pushDown: {
@@ -28,7 +36,17 @@ export default {
     }
   },
 
-  components: { SearchBar }
+  components: { SearchBar },
+
+  data() {
+    return {
+      uinfo: session.uinfo()
+    };
+  },
+
+  methods: {
+    logout: session.logout
+  }
 };
 </script>
 
@@ -40,37 +58,51 @@ export default {
   box-shadow: 0 2px 2px rgba(0, 0, 0, 0.1);
 }
 
-  .logo {
-    border-width: 1px;
-    border-style: solid;
-    border-top-color: #ff6c3f;
-    border-left-color: #ff6c3f;
-    border-right-color: #e95627;
-    border-bottom-color: #e95627;
-    background: rgb(185, 64, 27);
-    background: -moz-linear-gradient(0deg, rgba(185, 64, 27, 1) 0%, rgba(253, 82, 29, 1) 100%);
-    background: -webkit-linear-gradient(0deg, rgba(185, 64, 27, 1) 0%, rgba(253, 82, 29, 1) 100%);
-    background: linear-gradient(0deg, rgba(185, 64, 27, 1) 0%, rgba(253, 82, 29, 1) 100%);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#b9401b", endColorstr="#fd521d", GradientType=1);
-    /*-webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, .2);-moz-box-shadow: 0 1px 2px rgba(0, 0, 0, .2);box-shadow:*/
-    /*0 1px 2px rgba(0, 0, 0, .2);*/
-    display: inline-block;
-    width: 110px;
-    height: 43px;
-    vertical-align: top;
-    background-color: #fd521d;
-    background-image: url(../assets/logo.png);
-    background-repeat: no-repeat;
-    -webkit-background-size: contain;background-size: contain;
-    background-position: center center;
-  }
+.logo {
+  border-width: 1px;
+  border-style: solid;
+  border-top-color: #ff6c3f;
+  border-left-color: #ff6c3f;
+  border-right-color: #e95627;
+  border-bottom-color: #e95627;
+  background: rgb(185, 64, 27);
+  background: -moz-linear-gradient(
+    0deg,
+    rgba(185, 64, 27, 1) 0%,
+    rgba(253, 82, 29, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    0deg,
+    rgba(185, 64, 27, 1) 0%,
+    rgba(253, 82, 29, 1) 100%
+  );
+  background: linear-gradient(
+    0deg,
+    rgba(185, 64, 27, 1) 0%,
+    rgba(253, 82, 29, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#b9401b", endColorstr="#fd521d", GradientType=1);
+  /*-webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, .2);-moz-box-shadow: 0 1px 2px rgba(0, 0, 0, .2);box-shadow:*/
+  /*0 1px 2px rgba(0, 0, 0, .2);*/
+  display: inline-block;
+  width: 110px;
+  height: 43px;
+  vertical-align: top;
+  background-color: #fd521d;
+  background-image: url(../assets/logo.png);
+  background-repeat: no-repeat;
+  -webkit-background-size: contain;
+  background-size: contain;
+  background-position: center center;
+}
+
 .left {
   left: 130px;
-  width: 70%;
+  width: 60%;
 }
 
 .right {
-  width: 30%;
+  width: 40%;
 }
 
 .nav-item {
@@ -81,6 +113,7 @@ export default {
 .nav-item {
   color: #555;
   text-decoration: none;
+  cursor: pointer;
 }
 
 .tel {
---
+++ project/010-yangyangche/src/lib/session.js
@@ -0,0 +1,10 @@
+export default {
+  uinfo() {
+    return JSON.parse(localStorage.getItem('uinfo'));
+  },
+
+  logout(url) {
+    localStorage.removeItem('uinfo');
+    location.href = url || '/';
+  }
+};
